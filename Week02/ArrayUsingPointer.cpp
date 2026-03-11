30a767e1ca65ddfb9e080b0b54479196df6ca6c6
Sathvika Rednam <sathvika.rednam.309@gmail.com>
2026-03-11 21:56:04 +0530

    Add files via upload

---
+++ Week02/ArrayUsingPointer.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cout << "Enter size: ";
+    cin >> n;
+
+    int arr[n];
+    int *ptr = arr;
+
+    for(int i=0;i<n;i++)
+        cin >> *(ptr+i);
+
+    for(int i=0;i<n;i++)
+        cout << *(ptr+i) << " ";
+
+    return 0;
+}
