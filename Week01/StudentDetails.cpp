e4eff5d8841a369fd55ccc91bec3c9479defcbd0
Sathvika Rednam <sathvika.rednam.309@gmail.com>
2026-03-11 21:48:53 +0530

    Add files via upload

---
+++ Week01/StudentDetails.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    string name = "Gloria";
+    string branch = "CSE";
+    char section = 'A';
+
+    cout << "Name: " << name << endl;
+    cout << "Branch: " << branch << endl;
+    cout << "Section: " << section << endl;
+
+    return 0;
+}
