f8e18e242bc6aa78260edbb8d5bf6e4c7784225b
Sathvika Rednam <sathvika.rednam.309@gmail.com>
2026-03-11 22:01:45 +0530

    Add files via upload

---
+++ Week03/Linear search.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n,key;
+    cin >> n;
+
+    int arr[n];
+
+    for(int i=0;i<n;i++)
+        cin >> arr[i];
+
+    cout << "Enter key: ";
+    cin >> key;
+
+    for(int i=0;i<n;i++) {
+        if(arr[i]==key) {
+            cout << "Found at position " << i+1;
+            return 0;
+        }
+    }
+
+    cout << "Not Found";
+}
